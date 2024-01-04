5cd94e6b0c22708a69b0e105d0c8330515c2a2c8
UdithDas <udithdaskm@gmail.com>
2024-01-04 15:43:33 +0530

    first commit

---
+++ src/pages/Comment.jsx
@@ -0,0 +1,13 @@
+import { TextField, Typography } from '@mui/material';
+import React from 'react';
+
+
+const Comment = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Comment;
