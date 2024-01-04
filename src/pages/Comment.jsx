14f07194f991815dba1c88fd7136a159fa254469
Udithdas <127194106+UdithDas@users.noreply.github.com>
2024-01-04 22:56:35 +0530

    added

--- src/pages/Comment.jsx
+++ src/pages/Comment.jsx
@@ -1,11 +1,22 @@
-import { TextField, Typography } from '@mui/material';
+import { TextField, Typography,Button} from '@mui/material';
 import React from 'react';
 
 
 const Comment = () => {
     return (
-        <div>
-            
+        <div className='regs'>
+        <h1>Restuarant Registration</h1><br/>
+            <form>
+            <TextField id="outlined-basic" label="Restuarant Name" variant="outlined" /><br/><br/>
+            <TextField id="outlined-basic" label="Cuisine Type" variant="outlined" /><br/><br/>
+            <TextField id="outlined-basic" label="Contact no:" variant="outlined" /><br/><br/>
+            <TextField id="outlined-basic" label="Manager" variant="outlined" /><br/><br/>
+            <TextField id="outlined-basic" label="Country" variant="outlined" /><br/><br/>
+            <TextField id="outlined-basic" label="state" variant="outlined" /><br/><br/>
+            <TextField id="outlined-basic" label="District" variant="outlined" /><br/><br/>
+            <TextField id="outlined-basic" label="user id" variant="outlined" /><br/><br/>
+            <Button variant="outlined">Register</Button>
+            </form>
         </div>
     );
 };
